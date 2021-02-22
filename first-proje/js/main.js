2484a019110edc15818f2fdf6cb04fdfea1750e3
orxanAbdulla <orkhanaa@code.edu.az>
2021-02-22 09:35:36 -0800

    About,FAQ's

--- first-proje/js/main.js
+++ first-proje/js/main.js
@@ -1,4 +1,36 @@
 $(function(){
-    $('.parallax-window').parallax({ imageSrc: "img/parallax.jpg" });
     $('.timer').countTo();
+     wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+
+    $(document).on("scroll",window,function(){
+        if(!$("#navbar").hasClass("whiteing")){
+            if($(window).scrollTop() > 100){
+                $("#navbar").css({
+                 "position": "fixed",
+                 "background-color": "white"
+                 
+                })
+             }else{
+                 $("#navbar").css({
+                     "position": "absolute",
+                     "background-color": "transparent"
+                    })
+             }
+        }
+    })
+    if($(window).scrollTop() >100){
+        $("#navbar").css({
+            "position": "fixed",
+            "background-color": "white"
+        })
+    };
 })
